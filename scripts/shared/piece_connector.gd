21d441b633b4186e65a6bab3eac7b8faccae7eb0
CarlesRa <jcramos8282@gmail.com>
2025-11-23 02:15:28 +0100

    snap pieces ok, got to deactivate connectors

--- scripts/shared/piece_connector.gd
+++ scripts/shared/piece_connector.gd
@@ -3,11 +3,15 @@ extends Area2D
 class_name PieceConnector
 
 var parent_piece: WavePiece
+var is_active: bool
 var connector_id: int = 0
 var target_connector_id: int = 0
 var snap_position: Vector2
 
 @onready var connector_point := $Connector
 
-func _ready() -> void:
+func _ready() -> void:	
 	snap_position = connector_point.global_position
+
+func initialize_connector() -> void:
+	is_active = parent_piece.is_piece_connected
